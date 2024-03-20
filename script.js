6509e6c7bcfd33dbdf906c549d322fcce3248861
AlexVieira1 <157771387+AlexVieira1@users.noreply.github.com>
2024-03-20 12:37:59 -0400

    script

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function adicionar(item){
     var quantidade = document.getElementById("quantidade" +item)
     var total = document.getElementById("total" + item)
     qtd[item] +=1
-    total= Number.parseFloat(valorProduto[item]) *qtd[item]
+    total[item]= Number.parseFloat(valorProduto[item]) *qtd[item]
     quantidade.innerHTML = qtd[item]
     total.innerHTML = total[item].toFixed(2)
 }
